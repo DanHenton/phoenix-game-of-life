ad63f5a79966352500ba5eb79118c9c2aa6649cf
DanHenton <danjhenton@gmail.com>
2023-09-21 10:22:12 +1200

    init

---
+++ config/test.exs
@@ -0,0 +1,27 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :game_of_life, GameOfLife.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "game_of_life_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :game_of_life, GameOfLifeWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "b3uAjWcaSx7Y3i5JdiuD6z6fCEqLp2VPkzw4GEVJJ5z+WY7HisCbouqPdslGbqu2",
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
