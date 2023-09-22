599f087ddda464c741df4a8df2650197ff2eeb3d
DanHenton <danjhenton@gmail.com>
2023-09-22 16:57:00 +1200

    removed commented code

--- lib/game_of_life/experiments/experiment.ex
+++ lib/game_of_life/experiments/experiment.ex
@@ -18,14 +18,5 @@ defmodule GameOfLife.Experiments.Experiment do
     experiment
     |> cast(attrs, [:name, :width, :height, :grid])
     |> validate_required([:name, :width, :height])
-
-    # |> grid_reset()
   end
-
-  # defp grid_reset(changeset) do
-  #   if Map.has_key?(changeset.changes, :width) || Map.has_key?(changeset.changes, :height) do
-  #     changeset
-  #     |> put_change(:grid, GameOfLife.Services.Grid.dehydrate_grid(GameOfLife.Services.Grid.create_grid(changeset.changes[:width], changeset.changes[:height])))
-  #   end
-  # end
 end
